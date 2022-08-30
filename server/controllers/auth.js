ce42428456986733326a64f5e43f0abfdda047fb
MikeMi13 <mikemikemi1307@gmail.com>
2022-08-30 16:32:42 -0700

    email for account activation

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,4 +1,7 @@
+const User = require('../models/user');
 const AWS = require('aws-sdk');
+const jwt = require('jsonwebtoken');
+const {registerEmailParams} = require('../helpers/email');
 
 // AWS configuration
 AWS.config.update({
@@ -12,34 +15,34 @@ const SES = new AWS.SES({ apiVersion: '2010-12-01' });
 exports.register = (req, res) => {
     //console.log('REGISTER CONTROLLER', req.body);
     const { name, email, password } = req.body;
-    const params = {
-        Source: process.env.EMAIL_FROM,
-        Destination: {
-            ToAddresses: [email]
-        },
-        ReplyToAddresses: [process.env.EMAIL_TO],
-        Message: {
-            Body: {
-                Html: {
-                    Charset: 'UTF-8',
-                    Data: `<html><body><h1 style="color:red;">Hello ${name}</h1><p>Test email</p></body></html>`
-                }
-            },
-            Subject: {
-                Charset: 'UTF-8',
-                Data: 'Complete Your Registration'
-            }
-        }
-    };
 
-    const sendEmailOnRegisterPromise = SES.sendEmail(params).promise();
-    sendEmailOnRegisterPromise
-        .then(data => {
-            console.log('email submitted to SES', data);
-            res.send('Email sent');
-        })
-        .catch(error => {
-            console.log('SES email on register', error);
-            res.send('Email failed to send');
+    // check if user email already exists
+    User.findOne({ email }).exec((err, user) => {
+        if (user) {
+            return res.status(400).json({
+                error: 'Email is already taken.'
+            });
+        }
+        // generate token with username, email, and password
+        const token = jwt.sign({ name, email, password }, process.env.JWT_ACCOUNT_ACTIVATION, {
+            expiresIn: '3m'
         });
+
+        // send email
+        const params = registerEmailParams(email, token);
+        const sendEmailOnRegisterPromise = SES.sendEmail(params).promise();
+        sendEmailOnRegisterPromise
+            .then(data => {
+                console.log('Email submitted to SES:', data);
+                res.json({
+                    message: `Email has been sent to ${email}. Please follow the instructions to activate your email.`
+                });
+            })
+            .catch(error => {
+                console.log('SES email on register error:', error);
+                res.json({
+                    error: `We could not verify your email. Please try again later.`
+                });
+            });
+    });
 };
