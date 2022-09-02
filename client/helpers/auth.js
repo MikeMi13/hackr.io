71516ca3f539c571133c398af48e3abe1d5379c1
MikeMi13 <mikemikemi1307@gmail.com>
2022-09-02 16:13:34 -0700

    use SSR and getInitialProps to restrict access

--- client/helpers/auth.js
+++ client/helpers/auth.js
@@ -21,10 +21,29 @@ export const removeCookie = (key) => {
 
 // get from cookie
 // will be useful when we need to make requests to server with auth token
-export const getCookie = (key) => {
-    if (process.browser) {
-        return cookie.get(key);
+export const getCookie = (key, req) => {
+    // if (process.browser) {
+    //     return cookie.get(key);
+    // }
+    return process.browser ? getCookieFromBrowser(key) : getCookieFromServer(key, req);
+};
+
+export const getCookieFromBrowser = (key) => {
+    return cookie.get(key);
+};
+
+export const getCookieFromServer = (key, req) => {
+    if (!req.headers.cookie) {
+        return undefined;
     }
+    //console.log('req.headers.cookie:', req.headers.cookie);
+    let token = req.headers.cookie.split(';').find(cookie => cookie.trim().startsWith(`${key}=`));
+    if (!token) {
+        return undefined;
+    }
+    let tokenValue = token.split('=')[1];
+    //console.log('getCookieFromServer:', tokenValue);
+    return tokenValue;
 };
 
 // set local storage
