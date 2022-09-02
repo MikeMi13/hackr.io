ea537f164b880937f7207a1b9d9de884cc7b6a59
MikeMi13 <mikemikemi1307@gmail.com>
2022-09-02 16:55:22 -0700

    withUser and withAdmin HOC access restriction wrapper

--- client/pages/user/index.js
+++ client/pages/user/index.js
@@ -1,25 +1,6 @@
 import Layout from '../../components/Layout';
-import axios from 'axios';
-import {API} from '../../config';
-import { getCookie } from '../../helpers/auth';
+import withUser from '../withUser';
 
 const User = ({user}) => <Layout>{JSON.stringify(user)}</Layout>;
 
-User.getInitialProps = async (context) => {
-    const token = getCookie('token', context.req);
-    try {
-        const response = await axios.get(`${API}/user`, {
-            headers: {
-                authorization: `Bearer ${token}`,
-                contentType: 'application/json'
-            }
-        });
-        return {user: response.data};
-    } catch (err) {
-        if (err.response.status === 401) {
-            return {user: 'no user'};
-        }
-    }
-};
-
-export default User;
+export default withUser(User);
---
+++ client/pages/withAdmin.js
@@ -0,0 +1,47 @@
+import axios from 'axios';
+import {API} from '../config';
+import { getCookie } from '../helpers/auth';
+
+const withAdmin = (Page) => {
+    const withAdminUser = (props) => <Page {...props} />;
+    withAdminUser.getInitialProps = async (context) => {
+        const token = getCookie('token', context.req);
+        let user = null;
+        
+        if (token) {
+            try {
+                const response = await axios.get(`${API}/admin`, {
+                    headers: {
+                        authorization: `Bearer ${token}`,
+                        contentType: 'application/json'
+                    }
+                });
+                user = response.data;
+            } catch (err) {
+                if (err.response.status === 401) {
+                    user = null;
+                }
+            }
+        }
+
+        if (user === null) {
+            // redirect to home page
+            context.res.writeHead(302, {
+                Location: '/'
+            });
+            context.res.end();
+        } else {
+            return {
+                // Not all pages use getInitialProps.
+                // If a page is using getInitialProps, then wait for that to resolve (fetching data etc) and return.
+                // Otherwise just return without waiting for getInitialProps.
+                ...(Page.getInitialProps ? await Page.getInitialProps(context) : {}),
+                user,
+                token
+            };
+        }
+    };
+    return withAdminUser;
+};
+
+export default withAdmin;
