c20c3dd0431ab92e10c6309da425967dfe0d0461
MikeMi13 <mikemikemi1307@gmail.com>
2022-09-02 00:39:43 -0700

    auth(logged in user) and admin middleware

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -110,3 +110,38 @@ exports.login = (req, res) => {
 };
 
 exports.requireLogin = expressjwt({secret: process.env.JWT_SECRET});
+
+exports.authMiddleware = (req, res, next) => {
+    const authUserId = req.user._id; // made available by expressjwt
+    User.findOne({_id: authUserId}).exec((err, user) => {
+        if (err || !user) {
+            console.log(err);
+            return res.status(400).json({
+                error: 'User not found.'
+            });
+        }
+        // add new property
+        req.profile = user;
+        next();
+    });
+}
+
+exports.adminMiddleware = (req, res, next) => {
+    const adminUserId = req.user._id; // made available by expressjwt
+    User.findOne({_id: adminUserId}).exec((err, user) => {
+        if (err || !user) {
+            console.log(err);
+            return res.status(400).json({
+                error: 'User not found'
+            });
+        }
+        if (user.role != 'admin') {
+            return res.status(400).json({
+                error: 'Admin resource. Access denied.'
+            });
+        }
+        // add new property
+        req.profile = user;
+        next();
+    });
+}
