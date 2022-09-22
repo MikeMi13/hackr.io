c8f85e8471911ced1362e250ed01bbf8a5e9d14d
MikeMi13 <mikemikemi1307@gmail.com>
2022-09-22 14:47:46 -0700

    Most popular links in a given category

--- client/pages/links/[slug].js
+++ client/pages/links/[slug].js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Layout from '../../components/Layout';
 import Link from 'next/link';
 import axios from 'axios';
@@ -13,10 +13,21 @@ const Links = ({query, category, links, totalLinks, linksLimit, linksSkip}) => {
     const [limit, setLimit] = useState(linksLimit);
     const [skip, setSkip] = useState(linksSkip);
     const [size, setSize] = useState(totalLinks);
+    const [popular, setPopular] = useState([]);
+
+    useEffect(() => {
+        loadPopularLinks();
+    }, []);
+
+    const loadPopularLinks = async () => {
+        const response = await axios.get(`${API}/link/popular/${category.slug}`);
+        setPopular(response.data);
+    };
 
     const handleClick = async (linkId) => {
         const response = await axios.put(`${API}/click-count`, {linkId});
         loadUpdatedLinks();
+        loadPopularLinks();
     }
 
     const loadUpdatedLinks = async () => {
@@ -24,10 +35,39 @@ const Links = ({query, category, links, totalLinks, linksLimit, linksSkip}) => {
         setAllLinks(response.data.links);
     };
 
+    const listOfPopularLinks = () => (
+        popular.map((l, i) => (
+            <div key={i} className='row alert alert-secondary m-1 p-2'>
+                <div className='col-md-8' onClick={() => handleClick(l._id)}>
+                    <a href={l.url} target="_blank">
+                        <h5 className='pt-2'>
+                            {l.title}
+                        </h5>
+                        <h6 className='pt-2 text-danger' style={{fontSize: '12px'}}>
+                            {l.url}
+                        </h6>
+                    </a>
+                </div>
+                <div className='col-md-4 pt-2'>
+                    <span className='pull-right'>
+                        {moment(l.createdAt).fromNow()} by {l.postedBy.name}
+                    </span>
+                </div>
+                <div className='col-md-12'>
+                    <span className='badge text-dark'>
+                        {l.type} {l.medium}
+                    </span>
+                    {l.categories.map((c, i) => (<span key={i} className="badge text-success">{c.name}</span>))}
+                    <span className='badge text-secondary float-right'>{l.clicks} clicks</span>
+                </div>
+            </div>
+        ))
+    );
+
     const listOfLinks = () => (
         allLinks.map((link, i) => (
-            <div key={i} className='row alert alert-primary p-2'>
-                <div className='col-md-8' onClick={e => handleClick(link._id)}>
+            <div key={i} className='row alert alert-primary m-1 p-2'>
+                <div className='col-md-8' onClick={() => handleClick(link._id)}>
                     <a href={link.url} target="_blank">
                         <h5 className='pt-2'>{link.title}</h5>
                         <h6 className='pt-2 text-danger' style={{fontSize: '12px'}}>{link.url}</h6>
@@ -54,14 +94,6 @@ const Links = ({query, category, links, totalLinks, linksLimit, linksSkip}) => {
         setSkip(toSkip);
     };
 
-    // const loadMoreButton = () => {
-    //     return (
-    //         size > 0 && size >= limit && (
-    //             <button onClick={loadMore} className='btn btn-outline-primary btn-lg'>Load More</button>
-    //         )
-    //     )
-    // };
-
     return (
         <Layout>
             <div className='row'>
@@ -74,16 +106,6 @@ const Links = ({query, category, links, totalLinks, linksLimit, linksSkip}) => {
                 </div>
             </div>
             <br />
-            {/* {<div className='row'>
-                <div className='col-md-8'>
-                    {listOfLinks()}  
-                </div>
-                <div className='col-md-4'>
-                    <h2 className='lead'>Most Popular in {category.name}</h2>
-                </div>
-            </div>} */}
-            
-            {/*<div className='text-center pt-4 pb-5'>{loadMoreButton()}</div>*/}
             
             <InfiniteScroll
                 pageStart={0}
@@ -97,6 +119,7 @@ const Links = ({query, category, links, totalLinks, linksLimit, linksSkip}) => {
                     </div>
                     <div className='col-md-4'>
                         <h2 className='lead'>Most Popular in {category.name}</h2>
+                        {listOfPopularLinks()}
                     </div>
                 </div>
             </InfiniteScroll>
