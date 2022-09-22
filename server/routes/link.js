ec5acc583a8a447cacadcea870187bc46a68c065
MikeMi13 <mikemikemi1307@gmail.com>
2022-09-21 20:30:10 -0700

    canUpdateDeleteLink middleware

--- server/routes/link.js
+++ server/routes/link.js
@@ -6,7 +6,7 @@ const { linkCreateValidator, linkUpdateValidator } = require('../validators/link
 const { runValidation } = require('../validators/index');
 
 // controllers
-const { requireLogin, authMiddleware, adminMiddleware } = require('../controllers/auth');
+const { requireLogin, authMiddleware, adminMiddleware, canUpdateDeleteLink } = require('../controllers/auth');
 const { create, list, read, update, remove, clickCount } = require('../controllers/link');
 
 // routes (for CRUD)
@@ -14,7 +14,9 @@ router.post('/link', requireLogin, authMiddleware, linkCreateValidator, runValid
 router.post('/links', requireLogin, adminMiddleware, list);
 router.put('/click-count', clickCount);
 router.get('/link/:id', read);
-router.put('/link/:id', requireLogin, authMiddleware, linkUpdateValidator, runValidation, update);
-router.delete('/link/:id', requireLogin, authMiddleware, remove);
+router.put('/link/:id', requireLogin, authMiddleware, canUpdateDeleteLink, linkUpdateValidator, runValidation, update);
+router.put('/link/admin/:id', requireLogin, adminMiddleware, linkUpdateValidator, runValidation, update);
+router.delete('/link/:id', requireLogin, authMiddleware, canUpdateDeleteLink, remove);
+router.delete('/link/admin/:id', requireLogin, adminMiddleware, remove);
 
 module.exports = router;
