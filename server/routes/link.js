3281c384ece6b097bfb271943b52f6bf309b15cf
MikeMi13 <mikemikemi1307@gmail.com>
2022-09-22 13:35:14 -0700

    Most popular/trending links - server

--- server/routes/link.js
+++ server/routes/link.js
@@ -7,12 +7,14 @@ const { runValidation } = require('../validators/index');
 
 // controllers
 const { requireLogin, authMiddleware, adminMiddleware, canUpdateDeleteLink } = require('../controllers/auth');
-const { create, list, read, update, remove, clickCount } = require('../controllers/link');
+const { create, list, read, update, remove, clickCount, popular, popularInCategory } = require('../controllers/link');
 
 // routes (for CRUD)
 router.post('/link', requireLogin, authMiddleware, linkCreateValidator, runValidation, create);
 router.post('/links', requireLogin, adminMiddleware, list);
 router.put('/click-count', clickCount);
+router.get('/link/popular', popular);
+router.get('/link/popular/:slug', popularInCategory);
 router.get('/link/:id', read);
 router.put('/link/:id', requireLogin, authMiddleware, canUpdateDeleteLink, linkUpdateValidator, runValidation, update);
 router.put('/link/admin/:id', requireLogin, adminMiddleware, linkUpdateValidator, runValidation, update);
